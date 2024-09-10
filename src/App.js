0371715e30cc4b1a4836cbf57aeb331105764fee
Alvyn YANG <alvyn.yang@epitech.eu>
2024-09-11 00:26:50 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function App() {
       <CssBaseline />
       <AppBar position="static" elevation={0} sx={{ backgroundColor: 'transparent' }}>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
+          <Typography variant="h7" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
             My Webtoons
           </Typography>
         </Toolbar>
